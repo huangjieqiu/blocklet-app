a898bf3dd6c225c9d825d82e21a7baa3dfc8486f
huangjieqiu <617940459@qq.com>
2022-01-03 16:55:27 +0800

    update

--- src/utils/scroll-to.js
+++ src/utils/scroll-to.js
@@ -36,13 +36,18 @@ Math.easeInOutQuad = function(t, b, c, d) {
     let currentTime = 0
     duration = (typeof (duration) === 'undefined') ? 500 : duration
     var animateScroll = function() {
+      // increment the time
       currentTime += increment
+      // find the value with the quadratic in-out easing function
       var val = Math.easeInOutQuad(currentTime, start, change, duration)
+      // move the document.body
       move(val)
+      // do the animation unless its over
       if (currentTime < duration) {
         requestAnimFrame(animateScroll)
       } else {
         if (callback && typeof (callback) === 'function') {
+          // the animation is done so lets callback
           callback()
         }
       }
